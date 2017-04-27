e5014187d7105ebd5d3d2802e65741f00fc69133
LitoMore <litomore@gmail.com>
2017-04-27 15:24:21 +0800

    add photo.type and photo.isGif()

--- src/photo.js
+++ src/photo.js
@@ -7,6 +7,11 @@ class Photo {
     this.thumburl = photo.thumburl
     this.largeurl = photo.largeurl
     this.originurl = photo.largeurl.replace(/@.+\..+$/g, '')
+    this.type = this.originurl.match(/^.+\.(.+)$/)[1].toLowerCase()
+  }
+
+  isGif () {
+    return this.type === 'gif'
   }
 }
 
