8db72e961f7b0af0d24fc6f67cad5df9a561f576
LitoMore <litomore@gmail.com>
2018-11-24 16:18:12 +0800

    Add TypeScript declaration file

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+export = Fanfou;
+export as namespace Fanfou;
+
+declare class Fanfou {
+    constructor(opt?: Fanfou.FanfouOptions)
+    xauth(): any;
+    get(uri: string, params: object): any;
+    post(uri: string, params: object): any;
+    upload(uri: string, params: object): any;
+}
+
+declare namespace Fanfou {
+    export interface FanfouOptions {
+        consumerKey: string,
+        consumerSecret: string,
+        oauthToken?: string,
+        oauthTokenSecret?: string,
+        username?: string,
+        password?: string,
+        protocol?: string,
+        fakeHttps?: boolean,
+        apiDomain?: string,
+        oauthDomain?: string
+    }
+}
