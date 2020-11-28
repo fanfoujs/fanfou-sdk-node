660d45ebf3accf44ee357b2c9ec9e2d66b861f65
LitoMore <litomore@gmail.com>
2020-11-28 20:11:41 +0800

    Bump xo@0.35.0

--- index.d.ts
+++ index.d.ts
@@ -2,39 +2,39 @@ export = Fanfou;
 export as namespace Fanfou;
 
 declare class Fanfou {
-    consumerKey: string;
-    consumerSecret: string;
-    oauthToken: string;
-    oauthTokenSecret: string;
-    username: string;
-    password: string;
-    protocol: string;
-    apiDomain: string;
-    oauthDomain: string;
-    hooks: any;
-    constructor(opt?: Fanfou.FanfouOptions)
-    getRequestToken(): any;
-    getAccessToken(token :object): any;
-    xauth(): any;
-    get(uri: string, params: object): any;
-    post(uri: string, params: object): any;
-    upload(uri: string, params: object): any;
+	consumerKey: string;
+	consumerSecret: string;
+	oauthToken: string;
+	oauthTokenSecret: string;
+	username: string;
+	password: string;
+	protocol: string;
+	apiDomain: string;
+	oauthDomain: string;
+	hooks: any;
+	constructor(opt?: Fanfou.FanfouOptions)
+	getRequestToken(): any;
+	getAccessToken(token: Record<string, unknown>): any;
+	xauth(): any;
+	get(uri: string, parameters: Record<string, unknown>): any;
+	post(uri: string, parameters: Record<string, unknown>): any;
+	upload(uri: string, parameters: Record<string, unknown>): any;
 }
 
 declare namespace Fanfou {
-    export interface FanfouOptions {
-        consumerKey: string,
-        consumerSecret: string,
-        oauthToken?: string,
-        oauthTokenSecret?: string,
-        username?: string,
-        password?: string,
-        protocol?: string,
-        fakeHttps?: boolean,
-        apiDomain?: string,
-        oauthDomain?: string,
-        hooks?: {
-            baseString?: (str: string) => any;
-        }
-    }
+	export interface FanfouOptions {
+		consumerKey: string;
+		consumerSecret: string;
+		oauthToken?: string;
+		oauthTokenSecret?: string;
+		username?: string;
+		password?: string;
+		protocol?: string;
+		fakeHttps?: boolean;
+		apiDomain?: string;
+		oauthDomain?: string;
+		hooks?: {
+			baseString?: (string: string) => any;
+		};
+	}
 }
